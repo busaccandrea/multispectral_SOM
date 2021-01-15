1c5437b2dac8286379c1fc853566991b4d147e9f
Busacca <busaccandrea@gmail.com>
2021-01-15 10:15:49 +0100

    added reduction dim compatibility to som_main

--- som_main.py
+++ som_main.py
@@ -6,44 +6,61 @@ from PIL import Image
 import os
 from sklearn.preprocessing import MinMaxScaler
 import utilities
+import reduce_dim_spectra as reduce_dim
 
 if __name__ == '__main__':  
     # if this file exists, the SOM is already trained...
-    if os.path.isfile('clusters_5x5.npy'):
-        clusters = np.load('clusters_5x5.npy', allow_pickle=True)
+    # Define the map shape.
+    map_height = 4
+    map_width = 4
+######################################### SET THIS VAR BEFORE LAUNCH ########################################################    
+    reduction_method = 'nmf'                                                                                                #
+#############################################################################################################################
+    if os.path.isfile('clusters_' + reduction_method + '_' + str(map_height) + 'x' + str(map_width) + '.npy'):
+        clusters = np.load('clusters_' + reduction_method + '_' + str(map_height) + 'x' + str(map_width) + '.npy', allow_pickle=True)
         
         # ... so let's exports all images.
-        utilities.save_clusters_images(clusters= clusters)
+        utilities.save_clusters_images(clusters=clusters, base_filename=reduction_method)
 
-    else: 
-        # Define the map shape.
-        map_height = 5
-        map_width = 5
-
+    else:
         # initialize SOM 
         alpha_start = 0.6 # default value = 0.6
         seed = 2233 # default value = 2233
         nn = SOM(map_height, map_width, alpha_start=alpha_start, seed=seed)
         
         # load data 
-        data = sparse.load_npz('sparse_B.npz').toarray()
+        # data = sparse.load_npz('sparse_B.npz').toarray()
+        datafilename = 'data_' + reduction_method + '_8.npy'
+        data = np.load(datafilename)
 
         # initialize cluster matrix (shape: [#pixel, #cluster])
         clusters = np.zeros([data.shape[0], map_height*map_width], dtype=np.uint8)
-
         # train SOM. epochs = 0 means all dataset
         nn.fit(data=data, epochs=0, decay='hill')
 
-        with open('csv_pixel-cluster.csv', 'w', newline='') as csvfile:
-            csvwriter = csv.writer(csvfile, delimiter=' ')
-            for idx, row in enumerate(data):
-                row_winner, col_winner = nn.winner(row) # <--- get class and class coordinates --.
-                cluster = 5 * row_winner + col_winner   # <--------------------------------------'
-                csvwriter.writerow([idx, cluster])
+        print('clusters...')
+        # with open('pixel-cluster.csv', 'w', newline='') as csvfile:
+        #     csvwriter = csv.writer(csvfile, delimiter=' ')
+        #     for idx, row in enumerate(data):
+        #         # debugging
+        #         # print('idx=', idx)
+        #         row_winner, col_winner = nn.winner(row) # <--- get class and class coordinates ----------.
+        #         cluster = map_width * row_winner + col_winner   # <--------------------------------------'
+        #         # csvwriter.writerow([idx, cluster])
 
-                # this value is used to export cluster images
-                clusters[idx][cluster] = 255
+        #         # debugging
+        #         # print('cluster = ', cluster)
 
-        np.save('clusters_5x5.npy', clusters)
+        #         # this value is used to export cluster images
+        #         # clusters[idx][cluster] = 255    # cluster - 0 for 0-indexing dataset-label
+        #         clusters[idx][cluster-1] = 255  # cluster - 1 for 1-indexing dataset-label
+        for idx, row in enumerate(data):
+            row_winner, col_winner = nn.winner(row) # <--- get class and class coordinates ----------.
+            cluster = map_width * row_winner + col_winner   # <--------------------------------------'
+
+            clusters[idx][cluster-1] = 255  # cluster - 1 for 1-indexing dataset-label
+
+
+        np.save('clusters_' + reduction_method + '_' + str(map_height) + 'x' + str(map_width)  +'.npy', clusters)
         # saving SOM
         nn.save('som.p')
