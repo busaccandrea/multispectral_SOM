9b004801ec8e88702131bce111a53253c1f304c0
Busacca <busaccandrea@gmail.com>
2021-02-10 10:07:40 +0100

    som main: now outputs are values between 0 and 255.AfterTrainSavesImages
    fixed file names management

--- som_main.py
+++ som_main.py
@@ -1,3 +1,4 @@
+from numpy.lib.npyio import load
 from som import SOM
 from scipy import sparse
 import csv
@@ -7,60 +8,84 @@ import os
 from sklearn.preprocessing import MinMaxScaler
 import utilities
 import reduce_dim_spectra as reduce_dim
+import processing_data
 
 if __name__ == '__main__':  
     # if this file exists, the SOM is already trained...
     # Define the map shape.
-    map_height = 4
-    map_width = 4
+    map_height = 2
+    map_width = 3
 ######################################### SET THIS VAR BEFORE LAUNCH ########################################################    
-    reduction_method = 'nmf'                                                                                                #
+    reduction_method = 'pca'                                                                                                #
 #############################################################################################################################
-    if os.path.isfile('clusters_' + reduction_method + '_' + str(map_height) + 'x' + str(map_width) + '.npy'):
-        clusters = np.load('clusters_' + reduction_method + '_' + str(map_height) + 'x' + str(map_width) + '.npy', allow_pickle=True)
-        
+    # for NNMF
+    n_components = 6
+    smoothing = 10
+
+
+    if os.path.isfile('data/' + reduction_method + '/' + 'clusters_' + str(n_components) + '_' + str(smoothing) + '_' + str(map_height) + 'x' + str(map_width) +'.npy'):
+        clusters_perc = np.load('data/' + reduction_method + '/' + 'clusters_' + str(n_components) + '_' + str(smoothing) + '_' + str(map_height) + 'x' + str(map_width) +'.npy', allow_pickle=True)
+
         # ... so let's exports all images.
-        utilities.save_clusters_images(clusters=clusters, base_filename=reduction_method)
+        print('Saving images...')
+        utilities.save_clusters_images(clusters_perc=clusters_perc, folder=reduction_method + '/')
+        print('Done. Saving spectras...')
+        utilities.get_clusters_spectras(clusters_perc, filename= reduction_method, write_in_file=True)
+        print('Done.')
 
     else:
-        # initialize SOM 
-        alpha_start = 0.6 # default value = 0.6
-        seed = 2233 # default value = 2233
-        nn = SOM(map_height, map_width, alpha_start=alpha_start, seed=seed)
-        
-        # load data 
-        # data = sparse.load_npz('sparse_B.npz').toarray()
-        datafilename = 'data_' + reduction_method + '_8.npy'
-        data = np.load(datafilename)
+        if reduction_method == 'nmf':
+            # load data 
+            datafilename = 'data/' + reduction_method + '/W_' + str(n_components) + '_comp_smth_' + str(smoothing) + '.npy'
+        else:
+            datafilename = 'data/' + reduction_method + '/data_' + str(n_components) + '_comp_smth_' + str(smoothing) + '.npy'
+
+
+        if os.path.isfile(datafilename):
+            data = np.load(datafilename)
+            print('Loaded data.')
+        else: 
+            B = np.load('data/B.npy')
+            print('Loaded B matrix. Reducing data...')
+            if reduction_method == 'nmf':
+                data = reduce_dim.nnmf_based(data=B, smoothing=smoothing, n_components_=n_components, write_to_file=True)
+            else:
+                data = reduce_dim.pca_based(data=B, n_components_=n_components, write_to_file=True)
+            print("Done and saved.")
 
         # initialize cluster matrix (shape: [#pixel, #cluster])
-        clusters = np.zeros([data.shape[0], map_height*map_width], dtype=bool)
-        # train SOM. epochs = 0 means all dataset
-        nn.fit(data=data, epochs=0, decay='hill')
+        perc_clusters = np.zeros([data.shape[0], map_height*map_width], dtype=np.uint8)
+        percentages = np.zeros([data.shape[0], map_height*map_width], dtype=np.float32)
+        
+        # initialize SOM 
+        alpha_start = 1 # default value = 0.6
+        seed = 2233 # default value = 2233
+        nn = SOM(map_height, map_width, alpha_start=alpha_start, seed=seed)
+        som_file = 'som_' + reduction_method + str(n_components) + '_' + str(smoothing) + '_' + str(map_height) + 'x' + str(map_width) +'.p'
+        if os.path.isfile(som_file):
+            nn.load(filename=som_file)
+            print('Loaded som.')
+        else:
+            print('training som...')
+            # train SOM. epochs = 0 means all dataset
+            nn.fit(data=data, epochs=0, decay='hill')
+            nn.save(som_file)
 
-        print('clusters...')
-        # with open('pixel-cluster.csv', 'w', newline='') as csvfile:
-        #     csvwriter = csv.writer(csvfile, delimiter=' ')
-        #     for idx, row in enumerate(data):
-        #         # debugging
-        #         # print('idx=', idx)
-        #         row_winner, col_winner = nn.winner(row) # <--- get class and class coordinates ----------.
-        #         cluster = map_width * row_winner + col_winner   # <--------------------------------------'
-        #         csvwriter.writerow([idx, cluster])
-
-        #         # debugging
-        #         # print('cluster = ', cluster)
-
-        #         # this value is used to export cluster images
-        #         # clusters[idx][cluster] = 255    # cluster - 0 for 0-indexing dataset-label
-        #         clusters[idx][cluster-1] = 255  # cluster - 1 for 1-indexing dataset-label
+        print('\nclusters...')
         for idx, row in enumerate(data):
-            row_winner, col_winner = nn.winner(row) # <--- get class and class coordinates ----------.
-            cluster = map_width * row_winner + col_winner   # <--------------------------------------'
+            # row_winner, col_winner = nn.winner(row) # <--- get class and class coordinates ----------.
+            # cluster = map_width * row_winner + col_winner   # <--------------------------------------'
+            distances = np.sum((nn.map - row) ** 2, axis=2)
+            percentages[idx] = processing_data.data_clustering(distances)
+            perc_clusters[idx] = np.uint8(255 * percentages[idx])
+        # np.save('clusters_' + reduction_method + '_' + str(map_height) + 'x' + str(map_width)  +'.npy', clusters)
+        np.save('data/' + reduction_method + '/' + 'clusters_' + str(n_components) + '_' + str(smoothing) + '_' + str(map_height) + 'x' + str(map_width) +'.npy', perc_clusters)
 
-            clusters[idx][cluster-1] = 1  # cluster - 1 for 1-indexing dataset-label
+        # ... so let's exports all images.
+        print('Saving images...')
+        utilities.save_clusters_images(clusters_perc=perc_clusters, folder=reduction_method + '/')
+        print('Done. Saving spectras...')
+        utilities.get_clusters_spectras(perc_clusters, filename= reduction_method, write_in_file=True)
+        print('Done.')
 
-
-        np.save('clusters_' + reduction_method + '_' + str(map_height) + 'x' + str(map_width)  +'.npy', clusters)
-        # saving SOM
-        nn.save('som.p')
+        print(reduction_method, 'components=', n_components, 'smoothing=', smoothing, 'h=', map_height, 'w=', map_width)
