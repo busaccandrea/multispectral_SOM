7f3ddb96b19263d17780c5ee2cb4242c7fbbcd5d
Busacca <busaccandrea@gmail.com>
2021-02-19 10:45:15 +0100

    compare: compute_all can plot all metrics available. fixes in descript
    fix: ssim
    fix: save only the correlation index for spearman and pearson metrics

--- compare.py
+++ compare.py
@@ -1,3 +1,4 @@
+from PIL import Image
 import utilities
 import numpy as np
 from scipy.stats import pearsonr
@@ -6,25 +7,42 @@ from sklearn.metrics import mean_squared_error as mse
 from skimage.metrics import structural_similarity as ssim
 import glob
 from os.path import basename
+from matplotlib import pyplot as plt
 
 
 class Comparison(object):
     """ Description """
     def __init__(self, data):
         self.data = data
-        self.chem_el_files = glob.glob('data/DOggionoGiulia/*.tiff')
+        self.chem_el_files = glob.glob('data/DOggionoGiulia/PNG-ed/*.png')
         self.M = utilities.get_matrix_from_chem_el_tiffs(self.chem_el_files)
-        # mse results is a matrix where rows are the SOM result and cols are elemental samples. mse_results[i][j] is mse between each elemental 
+        
+        # this line are used to convert tiff images in png images and/or 
+        # self._normalize_M()
+
+        # mse results is a matrix where rows are the clustering results and cols are elemental samples. mse_results[i][j] is mse between each elemental 
         # sample and each SOM result
         self.mses = np.zeros((data.shape[1], len(self.chem_el_files)))
         # same of mse
         self.ssims = np.zeros((data.shape[1], len(self.chem_el_files)))
         #same of mse
-        self.pearsons = np.zeros((data.shape[1], len(self.chem_el_files), 2))
-        self.spearmans = np.zeros((data.shape[1], len(self.chem_el_files), 2))
+        self.pearsons = np.zeros((data.shape[1], len(self.chem_el_files)))
+        self.spearmans = np.zeros((data.shape[1], len(self.chem_el_files)))
     
+    def _normalize_M(self):
+        max = np.max(self.M)
+        self.M = self.M.clip(min=0)
+        for i, _ in enumerate(self.M):
+            self.M[i] = ((self.M[i])/max)*255
 
-    def compute_all(self, verbose = False):
+        # these lines are uset to convert tiff images in png images
+        # for j in range(0, self.M.shape[1]):
+        #     im = self.M[:, j].reshape((418, 418))
+        #     img = Image.fromarray(im)
+        #     img = img.convert("L")
+        #     img.save(self.chem_el_files[j] + '.PNG')
+
+    def compute_all(self, verbose = True):
         """ 
         Compute all metrics in this class between SOM results and D'Oggiono Giulia files at data/DOggionoGiulia
         """
@@ -32,16 +50,44 @@ class Comparison(object):
             arr = self.M[:, j]
             for i in range(self.data.shape[1]):
                 my_arr = self.data[:, i]
-                self.ssims[i, j] = ssim(my_arr.astype(np.float32), arr)
+                self.ssims[i, j] = ssim(my_arr.astype(np.float64), arr)
                 self.mses[i, j] = mse(my_arr, arr)
-                self.pearsons[i, j] = pearsonr(my_arr, arr)
-                self.spearmans[i, j] = spearmanr(my_arr, arr)
-                if verbose:
-                    print('Comparage between', basename(f), 'and', i)
-                    print('\t\tssim:', round(self.ssims[i, j], 4))
-                    print('\t\tpearson:', round(self.pearsons[i, j, 0], 4))
-                    print('\t\tMSE:', round(self.mses[i,j], 4))
-                    print('\t\tSpearman:', round(self.spearmans[i, j, 0], 4))
+                (corr, p_value) = pearsonr(my_arr, arr)
+                self.pearsons[i, j] = corr
+                (corr, p_value) =  spearmanr(my_arr, arr)
+                self.spearmans[i, j] = corr
+
+        if verbose:
+            for cluster in range(self.mses.shape[0]):
+                fig, axs = plt.subplots(2, 2)
+                fig.suptitle('Comparing ' + str(cluster) + ' with tiffs')
+                # pearson
+                axs[0, 0].plot(self.pearsons[cluster], 'tab:blue')
+                axs[0, 0].set_ylim([-1, 1])
+                axs[0, 0].set_title('pearson')
+
+                # spearman
+                axs[0, 1].plot(self.spearmans[cluster], 'tab:orange')
+                axs[0, 1].set_ylim([-1, 1])
+                axs[0, 1].set_title('spearman')
+
+                # mse
+                axs[1, 0].plot(self.mses[cluster], 'tab:green')
+                axs[1, 0].set_title('mse')
+
+                # ssim
+                axs[1, 1].plot(self.ssims[cluster], 'tab:red')
+                axs[1, 1].set_ylim([0, 1])
+                axs[1, 1].set_title('ssim')
+                
+                for ax in axs.flat:
+                    ax.set(xlabel='tiff images', ylabel='metric value')
+                
+                # show plot in fullscreen
+                manager = plt.get_current_fig_manager()
+                manager.window.showMaximized()
+
+                plt.show()
 
 
     def pearson_correlation(self, verbose = False):
@@ -95,18 +141,7 @@ class Comparison(object):
                 self.spearmans[i, j] = spearmanr(my_arr, arr)
                 if verbose:
                     print('Spearman between', basename(f), 'and', i, ':', round(self.spearmans[i, j, 0], 4))
-        
 
-
-
-clusters = np.load('results/campioni/som2x3/nmf/6comp/smth 15/clusters_6_15_2x3.npy')
-comp = Comparison(data=clusters)
-# compare.pearson_correlation(verbose=True)
-# compare.mse_(verbose=True)
-max = np.max(comp.M)
-min = np.min(comp.M)
-for i, _ in enumerate(comp.M):
-    comp.M[i] = ((comp.M[i] - min)/(max - min))*255
-
-
-comp.compute_all(verbose=True)
+# clusters = np.load('C:/Users/apost/Pavel Folder/VS Code folder/multispectral_SOM/data/cmeans/nmf/clusters_8comp_15smth_8groups.npy')
+# comp = Comparison(data=clusters)
+# comp.compute_all(verbose=True)
