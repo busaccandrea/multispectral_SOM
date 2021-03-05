8f175bcf33c3cdf25104b64dd7b68245fe213a0e
Busacca <busaccandrea@gmail.com>
2021-03-05 11:45:53 +0100

    som_main_model: minor fixes

--- som_main_model.py
+++ som_main_model.py
@@ -11,6 +11,7 @@ import utilities
 import reduce_dim_spectra as reduce_dim
 import processing_data
 from compare import Comparison
+import torch
 
 if __name__ == '__main__':  
     # if this file exists, the SOM is already trained...
@@ -29,7 +30,7 @@ if __name__ == '__main__':
         clusters_perc = np.load('datanpy', allow_pickle=True)
 
     else:
-        data = np.load('data/data_cut.npy')
+        data = np.load('data/codes.npy')
         # initialize cluster matrix (shape: [#pixel, #cluster])
         clusters = np.zeros([data.shape[0], map_height*map_width], dtype=np.uint8)
         percentages = np.zeros([data.shape[0], map_height*map_width], dtype=np.float32)
@@ -38,7 +39,7 @@ if __name__ == '__main__':
         alpha_start = 1 # default value = 0.6
         seed = 2233 # default value = 2233
         nn = SOM(map_height, map_width, alpha_start=alpha_start, seed=seed)
-        som_file = 'data/checkpoints/som_' + str(map_width) + '.p'
+        som_file = 'data/checkpoints/som_pavenc_' + str(map_height) + 'x' + str(map_width) + '.p'
         if os.path.isfile(som_file):
             nn.load(filename=som_file)
             print('Loaded som.')
@@ -49,22 +50,23 @@ if __name__ == '__main__':
             print('save')
             nn.save(som_file)
         
-        print('saved')
 
         model_filename = 'data/pavencoder/' +  'model.pt'
         checkpoint_filename = 'data/pavencoder/' + 'model_checkpoint.pt'
         if os.path.isfile(model_filename) and os.path.isfile(checkpoint_filename):
             print('Model and checkpoint files found. Loading.')
             [model, optimizer, checkpoint] = load_model_for_inference(model_filename, checkpoint_filename)
+            model.double()
 
         print('\nclusters...')
-        for idx, r in enumerate(data):
+        for idx, row in enumerate(data):
             # row_winner, col_winner = nn.winner(row) # <--- get class and class coordinates ----------.
             # cluster = map_width * row_winner + col_winner   # <--------------------------------------'
-            row = model.encoder(r)
             distances = np.sum((nn.map - row) ** 2, axis=2)
             percentages[idx] = processing_data.data_clustering(distances)
             clusters[idx] = np.uint8(255 * percentages[idx])
+        
+        utilities.check_existing_folder('data/som/pavencoder/')
         # np.save('clusters_' + reduction_method + '_' + str(map_height) + 'x' + str(map_width)  +'.npy', clusters)
         np.save('data/som/pavencoder/clusters_' + str(map_height) + 'x' + str(map_width) +'.npy', clusters)
 
